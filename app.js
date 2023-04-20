6942ea3024b0ffe68062b0b897cba4f537c4c173
JingWang <wangjing061993@gmail.com>
2023-04-20 23:24:14 +0200

    prova

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const myCanvas = document.getElementById('my-canvas');
 const ctx = myCanvas.getContext('2d');
 
-const point = {
-    x: 300,
-    y: 300
+
+
+for (let i = 0; i < 100; i++) {
+    const point = {
+     x: Math.random() * 600,
+     y: Math.random() * 600
+    }
+    const red = Math.floor(Math.random() * 556);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    const alpha = Math.random();
+    
+    point.color = `rgba(${red},${green},${blue},${alpha})`
+   
+    setInterval(() => {
+
+        const randomX = (Math.random()* 4)-2;
+        const randomY = (Math.random()* 4)-2;
+     
+        ctx.strokeStyle= point.color
+         ctx.strokeRect(point.x, point.y, 1,1);
+         point.x += randomX;
+         point.y += randomY;
+     }, 1);
 }
 
 
 
-setInterval(() => {
-
-   const randomX = (Math.random()*4)-2;
-   const randomY = (Math.random()*4)-2;
-
-    ctx.fillStyle = 'red'
-    ctx.fillRect(point.x, point.y, 2, 2);
-    point.x += randomX;
-    point.y += randomY;
-}, 1);
---
+++ app_old2.js
@@ -0,0 +1,31 @@
+const myCanvas = document.getElementById('my-canvas');
+const ctx = myCanvas.getContext('2d');
+
+
+
+for (let i = 0; i < 100; i++) {
+    const point = {
+        x: 300,
+        y: 300
+    }
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    
+    point.color = `rgb(${red},${green},${blue})`
+
+    setInterval(() => {
+
+        const randomX = (Math.random()*4)-2;
+        const randomY = (Math.random()*4)-2;
+     
+         ctx.fillStyle = point.color
+         ctx.fillRect(point.x, point.y, 2, 2);
+         point.x += randomX;
+         point.y += randomY;
+     }, 1);
+}
+
+
+
+
